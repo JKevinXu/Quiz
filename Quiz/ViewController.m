2c7fea045fbc957def3995e625164062f0324c61
Jian (Kevin) Xu <Kevin@XuJians-MacBook.local>
2015-12-29 13:21:42 -0800

    A simple iOS app for doing quiz by showing questions and answers.

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -10,12 +10,45 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@property (nonatomic) NSArray *questions;
+@property (nonatomic) NSArray *answers;
+@property (nonatomic) int currentQuestionIndex;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == self.questions.count){
+        self.currentQuestionIndex = 0;
+    }
+
+
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.questions = @[ @"From what is cognac made?",
+                        @"What is 7+7?",
+                        @"What is the capital of Vermont?"];
+    
+    self.answers = @[ @"Grapes",
+                      @"14",
+                      @"Montpelier"];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
